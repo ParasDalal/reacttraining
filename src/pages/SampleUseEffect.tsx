e4ad39408f305358bed3770a8fa6c31624e3caf1
Paras Dalal <info@dalalinc.com>
2021-10-17 21:32:10 -0400

    useEffect

--- src/pages/SampleUseEffect.tsx
+++ src/pages/SampleUseEffect.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SampleUseEffect() {
+  const [value, setValue] = useState("Initial");
+
+  function sleep(milliseconds: number) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+  useEffect(() => {
+    sleep(2000);
+    setValue("Now Changed");
+    return () => {
+      // Clean up the subscription
+      //subscription.unsubscribe();
+    };
+  }, [value]);
   return (
     <div>
-      <div>useEffect</div>
+      <div>This is the value: {value}</div>
+      <br />
+      <button onClick={() => setValue("Button pressed")}>Press Me</button>
     </div>
   );
 }
